c6476a8728f0b543eec8cbf067a0c8dbee34eb65
nattan <fnatanieljunior@gmail.com>
2024-02-21 18:20:15 -0300

    tabBar estilizada, icons adicionados, página de home feita,header feito.

--- interfaceTikTok/src/pages/Home/index.js
+++ interfaceTikTok/src/pages/Home/index.js
@@ -1,15 +1,51 @@
-import { View,StyleSheet,Text } from 'react-native';
+import { View,StyleSheet,Text,TouchableOpacity, Platform, StatusBar} from 'react-native';
 
 export default function Home() {
  return (
    <View style={styles.container}>
-     <Text>Página de Home</Text> 
+
+    <View style={styles.labels}>
+      <TouchableOpacity>
+        <Text style={[styles.labelText, {color:"#DDD"}]}>Seguindo</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={[styles.labelText, {color:"#FFF"}]}>Pra Você</Text>
+        <View style={styles.indicator}></View>
+      </TouchableOpacity>
+
+    </View>
+     
    </View>
   );
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex:1
+        flex:1,
+        backgroundColor: "#000"
+    },
+    labels:{
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 8,
+      position: "absolute",
+      top: 6,
+      left: 0,
+      right: 0,
+      marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 55,
+      zIndex: 99
+    },
+    labelText:{
+      fontSize: 16,
+      fontWeight: "500",
+      marginBottom: 4
+    },
+    indicator:{
+      backgroundColor: "#FFF",
+      width: 32,
+      height: 2,
+      alignSelf: "center"
     }
 })
--- interfaceTikTok/src/routes/index.js
+++ interfaceTikTok/src/routes/index.js
@@ -4,37 +4,89 @@ import Search from '../pages/Search'
 import Inbox from '../pages/Inbox'
 import New from '../pages/New'
 import Profile from '../pages/Profile'
+import ButtonNew from '../components/ButtonNew'
+
+import {Ionicons} from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
 
 export function Routes(){
   return(
    <Tab.Navigator screenOptions={{
-    headerShown: false
+    headerShown: false,
+    tabBarShowLabel: false,
+    tabBarStyle:{ // cor da tabBar
+      backgroundColor: "#000",
+      borderTopWidth: 0,
+    },
+
+    tabBarActiveTintColor:"#FFF" // cor dos icons na tabBar quando clicar neles
    }}>
     <Tab.Screen 
      name="Home"
      component={Home}
+     options={{
+      tabBarIcon: ({focused, color, size})=>{
+        if(focused){
+          return <Ionicons name="home" color={color} size={size}/>
+        }
+        return <Ionicons name="home-outline" color={color} size={size}/>
+      }
+     }}
     />
 
     <Tab.Screen 
      name="Search"
      component={Search}
-    />
+     options={{
+      tabBarIcon: ({focused, color, size})=>{
+        if(focused){
+          return <Ionicons name="search" color={color} size={size}/>
+        }
+        return <Ionicons name="search-outline" color={color} size={size}/>
+      }
+     }}
 
-    <Tab.Screen 
-     name="Inbox"
-     component={Inbox}
     />
 
     <Tab.Screen 
      name="New"
      component={New}
+     options={{
+      tabBarIcon:({size}) => {
+        return <ButtonNew size={size}/>
+      }
+     }}
+     
     />
+
+
+    <Tab.Screen 
+     name="Inbox"
+     component={Inbox}
+     options={{
+      tabBarIcon: ({focused, color, size})=>{
+        if(focused){
+          return <Ionicons name="chatbox-ellipses" color={color} size={size}/>
+        }
+        return <Ionicons name="chatbox-ellipses-outline" color={color} size={size}/>
+      }
+     }}
+    />
+
+   
 
     <Tab.Screen 
      name="Profile"
      component={Profile}
+     options={{
+      tabBarIcon: ({focused, color, size})=>{
+        if(focused){
+          return <Ionicons name="person" color={color} size={size}/>
+        }
+        return <Ionicons name="person-outline" color={color} size={size}/>
+      }
+     }}
     />
 
   
