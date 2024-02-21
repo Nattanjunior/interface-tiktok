0132b7f271b1c25dd3c9ae6f3cd65545de85a974
nattan <fnatanieljunior@gmail.com>
2024-02-21 19:01:08 -0300

    componente do feed criado, para mostrar os videos, descrições e arrobas, começando a estilizar.

--- interfaceTikTok/src/pages/Home/index.js
+++ interfaceTikTok/src/pages/Home/index.js
@@ -1,6 +1,28 @@
-import { View,StyleSheet,Text,TouchableOpacity, Platform, StatusBar} from 'react-native';
+import { View,StyleSheet,Text,TouchableOpacity, Platform, StatusBar, FlatList} from 'react-native';
 
+import FeedItem from '../../components/FeedItem';
 export default function Home() {
+
+  const feedItem = [
+    {
+      id: 1,
+      video: 'https://i.imgur.com/Cnz1CPK.mp4',
+      name: '@sujeitoprogramador',
+      description: 'Criando o ShortDev do zero com RN'
+    },
+    {
+      id: 2,
+      video: 'https://i.imgur.com/E0tK2bY.mp4',
+      name: '@henriquesilva',
+      description: 'Fala turma, estou aprendendo React Native com sujeito programador'
+    },
+    {
+      id: 3,
+      video: 'https://i.imgur.com/mPFvFyX.mp4',
+      name: '@sujeitoprogramador',
+      description: 'Aprendendo a trabalhar com Drag and Drop no React Native'
+    }
+  ]
  return (
    <View style={styles.container}>
 
@@ -15,6 +37,12 @@ export default function Home() {
       </TouchableOpacity>
 
     </View>
+
+
+    <FlatList 
+      data={feedItem}
+      renderItem={({item})=> <FeedItem data={item}/>}
+    />
      
    </View>
   );
